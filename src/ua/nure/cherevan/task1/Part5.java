60a5f42957731d59dd40b1d461e8b83eef99cef0
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-03-05 12:49:46 +0200

    Create code of Part5

---
+++ src/ua/nure/cherevan/task1/Part5.java
@@ -0,0 +1,48 @@
+package ua.nure.cherevan.task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Part5 {
+
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int[] primeNumbers = getPrimeNumbers(n);
+
+        String outStr = "";
+        for (int i = 0; i < primeNumbers.length; i++) {
+            outStr += primeNumbers[i];
+
+            if (i == primeNumbers.length - 1) break;
+
+            outStr += " ";
+        }
+
+        System.out.println(outStr);
+    }
+
+    public static int[] getPrimeNumbers(int n) {
+        boolean[] numbers = new boolean[n];
+
+        for (int i = 2; i <= n; i++){
+            numbers[i - 1] = true;
+        }
+
+        for (int div = 2; div < n/2; div++) {
+            for (int num = div*div; num  < n; num += div) {
+                numbers[num - 1] = false;
+            }
+        }
+
+        List<Integer> primeNumbers = new ArrayList<Integer>(n/2 + 1);
+
+        for (int i = 1; i <= n; i++){
+            if(numbers[i - 1] == true) {
+                primeNumbers.add(i);
+            }
+        }
+
+        return primeNumbers.stream().mapToInt(i->i).toArray();
+    }
+}
