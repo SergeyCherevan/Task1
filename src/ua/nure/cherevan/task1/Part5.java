4a0197e76a6afc2f2d8c32e0ef5494eb25a5c0e4
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-03-24 13:55:45 +0200

    Add isPrime() method

--- src/ua/nure/cherevan/task1/Part5.java
+++ src/ua/nure/cherevan/task1/Part5.java
@@ -22,23 +22,32 @@ public class Part5 {
         System.out.println(outStr);
     }
 
+    public static boolean isPrime(int n) {
+
+        for (int i = 2; i < n/2; i++){
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+
     public static int[] getPrimeNumbers(int n) {
-        boolean[] numbers = new boolean[n];
+        boolean[] isNumbersPrime = new boolean[n];
 
         for (int i = 2; i <= n; i++){
-            numbers[i - 1] = true;
+            isNumbersPrime[i - 1] = true;
         }
 
         for (int div = 2; div < n/2; div++) {
-            for (int num = div*div; num  < n; num += div) {
-                numbers[num - 1] = false;
+            for (int num = div*div; num  <= n; num += div) {
+                isNumbersPrime[num - 1] = false;
             }
         }
 
         List<Integer> primeNumbers = new ArrayList<Integer>(n/2 + 1);
 
         for (int i = 1; i <= n; i++){
-            if(numbers[i - 1] == true) {
+            if(isNumbersPrime[i - 1] == true) {
                 primeNumbers.add(i);
             }
         }
